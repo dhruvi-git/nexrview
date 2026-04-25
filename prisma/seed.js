26192cdd4b2e2fbc13abc84c252ea9f0a6fda2e1
Dhruvi Jain <dhruvijain2019@gmail.com>
2026-04-25 18:39:03 +0530

    added dummy data

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../lib/generated/prisma/client/index.js";
+import { PrismaClient } from "../lib/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
