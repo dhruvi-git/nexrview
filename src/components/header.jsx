0d99b90b691c0fbd22037dc5f9c585cbd0e00a80
Dhruvi Jain <dhruvijain2019@gmail.com>
2026-04-28 10:06:07 +0530

    fixed naming issues

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ const Header = async () => {
       <Link href="/">
         <Image
           src="/logo.png"
-          alt="Prept Logo"
+          alt="Nexrview Logo"
           width={100}
           height={100}
           className="h-11 w-auto"
