2ccacc455d799e7c4284b8574be4db3cf1d88e3e
Dhruvi Jain <dhruvijain2019@gmail.com>
2026-05-15 10:43:38 +0530

    refractor backend

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,20 +1,8 @@
 "use server";
 
-import { currentUser } from "@clerk/nextjs/server";
-import { db } from "@/lib/prisma";
+import { withAuth } from "@/lib/action-utils";
+import { UserService } from "@/services/user.service";
 
-export const getCurrentUser = async () => {
-  const user = await currentUser();
-  if (!user) return null;
-
-  return db.user.findUnique({
-    where: { clerkUserId: user.id },
-    select: {
-      role: true,
-      name: true,
-      title: true,
-      company: true,
-      imageUrl: true,
-    },
-  });
-};
+export const getCurrentUser = withAuth(async ({ clerkUser }) => {
+  return UserService.getCurrentUserProfile(clerkUser.id);
+});
